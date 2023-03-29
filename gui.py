9c27707ed6736cd2d2a641e0e18c71a613be3117
Devenir-Glorieux <sgnikulin@hotmail.com>
2023-03-29 16:33:12 +0200

    gui

---
+++ gui.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+layout = [
+    [sg.Text('Глубина перфорации TVD (м)     '), sg.InputText(size=(8, 0.5))
+     ],
+    [sg.Text('Диаметр лифта НКТ (мм)            '), sg.InputText(size=(8, 0.5))
+     ],
+    [sg.Text('Длина лифта НКТ (м)                  '), sg.InputText(size=(8, 0.5))
+     ],
+    [sg.Text('Расход/дебет (м3/мин)               '), sg.InputText(size=(8, 0.5)), sg.Checkbox ('Нагнетательная')
+     ],
+    [sg.Text('Плотность флюида (кг/м3)           '), sg.InputText(size=(8, 0.5)), sg.Checkbox ('Ньютоновская жидкость')
+     ],
+    [sg.Text('Значения вискозиметра 300rpm  '), sg.InputText(size=(8, 0.5))
+     ],
+    [sg.Text('Значения вискозиметра 3rpm      '), sg.InputText(size=(8, 0.5)), 
+     ],
+    #[sg.Button('Расчет')]
+    [sg.Output(size=(88, 2))],
+    [sg.Submit('Расчет')]
+]
+window = sg.Window('Расчет забойного давления', layout)
+while True:                             # The Event Loop
+    event, values = window.read()
+    # print(event, values) #debug
+    if event in (None, 'Exit', 'Cancel'):
+        break
+
+
+
