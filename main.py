a0c3d5d75ef4c0e820619f7f3bc1a513f6fe34b9
Devenir-Glorieux <sgnikulin@hotmail.com>
2023-03-29 22:39:16 +0200

    29.03@

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import math
 print ('Скважина добывающая? (Y/N) ', end=" ")
 type_well = input ()
 if type_well == "Y":
-        print ('Глубина перфорации TVD (м) = ', end=" ")
+        print ('Глубина перфорации TVD (m) = ', end=" ")
         tvd = input ()
         tvd_1 = int(tvd)
 
-        print ('Диаметр лифта НКТ (м) = ', end=" ")     
+        print ('Диаметр лифта НКТ (m) = ', end=" ")     
         pipe_diametr = input ()
         pipe_diametr_1 = float(pipe_diametr)
 
-        print ('Длина лифта НКТ (м) = ', end=" ")           
+        print ('Длина лифта НКТ (m) = ', end=" ")           
         pipe_lenght = input ()
         pipe_lenght_1 = int(pipe_lenght)
 
@@ -19,11 +19,11 @@ if type_well == "Y":
         absolute_pipe_roughness = input ()
         absolute_pipe_roughness_1 = float(absolute_pipe_roughness)
 
-        print ('Расход/дебет (м3/сек) = ', end=" ")  
+        print ('Расход/дебет (m3/sec) = ', end=" ")  
         inj_rate = input ()
         inj_rate_1 = float(inj_rate)
 
-        print ('Плотность флюида (кг/м3) = ', end=" ") 
+        print ('Плотность флюида (kg/m3) = ', end=" ") 
         density = input ()
         density_1 = int(density)
 
@@ -31,13 +31,44 @@ if type_well == "Y":
         fluid_type = input ()
         if fluid_type == "Y":
             velocity = (inj_rate_1 / (math.pi * (pipe_diametr_1 / 2) **2 ))
-            print ('Скорость потока = ', velocity)
-        else: print ('ERROR')
+            dyn_visc = 1
+            re = (density_1 * pipe_diametr_1 * velocity) / (dyn_visc / 1000)
+            relative_roughness = (absolute_pipe_roughness_1 / pipe_diametr_1)
+            f = (-1.8 * math.log10((6.9/re) + (relative_roughness/3.7)**1.11))**-2
+            friction = f * pipe_lenght_1 * density_1 * ((velocity**2)/pipe_diametr_1)/100000
+            print ('Скорость потока = ', velocity, 'm/sec')
+            print ('Число Рейнольдса = ', re)
+            print ('Относительная шероховатость = ', relative_roughness)
+            print ('Коэффициент трения = ', f)
+            print ('Потери на трение = ', friction, 'bar')
+
+        else: 
+            print ('Показания вискозиметра при 300rpm = ', end=" ")
+            mud_visc_300rpm = input ()
+            mud_visc_300rpm_1 = int(mud_visc_300rpm)
+
+            print ('Показания вискозиметра при 3rpm = ', end=" ")
+            mud_visc_3rpm = input ()
+            mud_visc_3rpm_1 = int(mud_visc_3rpm)
+
+            velocity = (inj_rate_1 / (math.pi * (pipe_diametr_1 / 2) **2 ))
+            n = 0.5 * math.log (mud_visc_300rpm_1 / mud_visc_3rpm_1)
+            k = (5.11 * mud_visc_300rpm_1)/(511000000**n)
+            dyn_visc = k * 1000 * velocity ** n
+            re = (density_1 * pipe_diametr_1 * velocity) / (dyn_visc / 1000)
+            relative_roughness = (absolute_pipe_roughness_1 / pipe_diametr_1)
+            f = (-1.8 * math.log10((6.9/re) + (relative_roughness/3.7)**1.11))**-2
+            friction = f * pipe_lenght_1 * density_1 * ((velocity**2)/pipe_diametr_1)/100000
+             
+            print ('Скорость потока = ', velocity, 'm/sec')
+            print ('Индекс динамики потока, n = ', n)
+            print ('Коэффициент вязкости, K = ', k)
+            print ('Число Рейнольдса = ', re)
+            print ('Относительная шероховатость = ', relative_roughness)
+            print ('Коэффициент трения = ', f)
+            print ('Потери на трение = ', friction, 'bar')
+            
+        
 else: print ('ERROR')
     
 
-# mud_visc_300rpm = input ('Значения вискозиметра 300rpm = ')
-# mud_visc_3rpm = input ('Значения вискозиметра 3rpm = ')
-
-#print (tvd, pipe_diametr, pipe_lenght, inj_rate, densety, mud_visc_300rpm, mud_visc_3rpm)
-#print("Hello World", end=" ")
