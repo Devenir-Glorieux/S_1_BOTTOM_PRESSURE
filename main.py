04aa1b36a3d7a7afad4e75b5adfc15aec992a5b5
Devenir-Glorieux <sgnikulin@hotmail.com>
2023-03-30 11:47:45 +0200

    production well

--- main.py
+++ main.py
@@ -2,71 +2,63 @@ import math
 
 print ('Скважина добывающая? (Y/N) ', end=" ")
 type_well = input ()
-if type_well == "Y":
+if type_well == "Y" or type_well == "y":
         print ('Глубина перфорации TVD (m) = ', end=" ")
-        tvd = input ()
-        tvd_1 = int(tvd)
+        tvd = int(input ())
 
         print ('Диаметр лифта НКТ (m) = ', end=" ")     
-        pipe_diametr = input ()
-        pipe_diametr_1 = float(pipe_diametr)
+        pipe_diametr = float(input ())
 
         print ('Длина лифта НКТ (m) = ', end=" ")           
-        pipe_lenght = input ()
-        pipe_lenght_1 = int(pipe_lenght)
+        pipe_lenght = int(input ())
 
         print ('Абсолютная шероховатость труб (0.00002 по умолчанию) =  ', end=" ")  
-        absolute_pipe_roughness = input ()
-        absolute_pipe_roughness_1 = float(absolute_pipe_roughness)
+        absolute_pipe_roughness = float(input ())
 
         print ('Расход/дебет (m3/sec) = ', end=" ")  
-        inj_rate = input ()
-        inj_rate_1 = float(inj_rate)
+        inj_rate = float(input ())
 
         print ('Плотность флюида (kg/m3) = ', end=" ") 
-        density = input ()
-        density_1 = int(density)
+        density = int(input ())
 
         print ('Это Ньютоновская жидкость? (Y/N) ', end=" ")
         fluid_type = input ()
-        if fluid_type == "Y":
-            velocity = (inj_rate_1 / (math.pi * (pipe_diametr_1 / 2) **2 ))
+        if fluid_type == "Y" or fluid_type == "y":
+            velocity = (inj_rate / (math.pi * (pipe_diametr / 2) **2 ))
             dyn_visc = 1
-            re = (density_1 * pipe_diametr_1 * velocity) / (dyn_visc / 1000)
-            relative_roughness = (absolute_pipe_roughness_1 / pipe_diametr_1)
+            re = (density * pipe_diametr * velocity) / (dyn_visc / 1000)
+            relative_roughness = (absolute_pipe_roughness / pipe_diametr)
             f = (-1.8 * math.log10((6.9/re) + (relative_roughness/3.7)**1.11))**-2
-            friction = f * pipe_lenght_1 * density_1 * ((velocity**2)/pipe_diametr_1)/100000
-            print ('Скорость потока = ', velocity, 'm/sec')
-            print ('Число Рейнольдса = ', re)
-            print ('Относительная шероховатость = ', relative_roughness)
-            print ('Коэффициент трения = ', f)
-            print ('Потери на трение = ', friction, 'bar')
+            friction = f * pipe_lenght * density * ((velocity**2)/pipe_diametr)/100000
+            print ('Скорость потока = ', round(velocity, 4), 'm/sec')
+            print ('Число Рейнольдса = ', round(re, 4))
+            print ('Относительная шероховатость = ', round(relative_roughness, 4))
+            print ('Коэффициент трения = ', round(f, 4))
+            print ('Потери на трение = ', round(friction, 4), 'bar')
 
         else: 
             print ('Показания вискозиметра при 300rpm = ', end=" ")
-            mud_visc_300rpm = input ()
-            mud_visc_300rpm_1 = int(mud_visc_300rpm)
+            mud_visc_300rpm = int(input ())
 
             print ('Показания вискозиметра при 3rpm = ', end=" ")
-            mud_visc_3rpm = input ()
-            mud_visc_3rpm_1 = int(mud_visc_3rpm)
+            mud_visc_3rpm = int(input ())
 
-            velocity = (inj_rate_1 / (math.pi * (pipe_diametr_1 / 2) **2 ))
-            n = 0.5 * math.log (mud_visc_300rpm_1 / mud_visc_3rpm_1)
-            k = (5.11 * mud_visc_300rpm_1)/(511000000**n)
+            velocity = (inj_rate / (math.pi * (pipe_diametr / 2) **2 ))
+            n = 0.5 * math.log (mud_visc_300rpm / mud_visc_3rpm)
+            k = (5.11 * mud_visc_300rpm)/(511000000**n)
             dyn_visc = k * 1000 * velocity ** n
-            re = (density_1 * pipe_diametr_1 * velocity) / (dyn_visc / 1000)
-            relative_roughness = (absolute_pipe_roughness_1 / pipe_diametr_1)
+            re = (density * pipe_diametr * velocity) / (dyn_visc / 1000)
+            relative_roughness = (absolute_pipe_roughness / pipe_diametr)
             f = (-1.8 * math.log10((6.9/re) + (relative_roughness/3.7)**1.11))**-2
-            friction = f * pipe_lenght_1 * density_1 * ((velocity**2)/pipe_diametr_1)/100000
+            friction = f * pipe_lenght * density * ((velocity**2)/pipe_diametr)/100000
              
-            print ('Скорость потока = ', velocity, 'm/sec')
-            print ('Индекс динамики потока, n = ', n)
-            print ('Коэффициент вязкости, K = ', k)
-            print ('Число Рейнольдса = ', re)
-            print ('Относительная шероховатость = ', relative_roughness)
-            print ('Коэффициент трения = ', f)
-            print ('Потери на трение = ', friction, 'bar')
+            print ('Скорость потока = ', round(velocity, 4), 'm/sec')
+            print ('Индекс динамики потока, n = ', round(n, 4))
+            print ('Коэффициент вязкости, K = ', round(k, 4))
+            print ('Число Рейнольдса = ', round(re, 4))
+            print ('Относительная шероховатость = ', round(relative_roughness, 4))
+            print ('Коэффициент трения = ', round(f, 4))
+            print ('Потери на трение = ', round(friction, 4), 'bar')
             
         
 else: print ('ERROR')
